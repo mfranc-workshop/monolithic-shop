4b4b999f360e78300ed67abf0d7799a6448e9f8c
michal-franc <lam.michal.franc@gmail.com>
2017-03-18 13:24:20 +0000

    orders + reports functionality

--- GithubDashboard/Controllers/ReportsController.cs
+++ GithubDashboard/Controllers/ReportsController.cs
@@ -1,11 +1,14 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Threading;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GithubDashboard.Controllers
 {
     public class ReportsController : Controller
     {
+        [Route("/reports")]
         public IActionResult Index()
         {
+            Thread.Sleep(2000);
             return View();
         }
     }
